53fe3cd90ce1f5356b31d556b498df9e149f8901
LeGitHubDeTai <65470169+LeGitHubDeTai@users.noreply.github.com>
2026-02-14 16:06:14 +0100

    Fix: nginx

--- n8n/nginx-entrypoint.sh
+++ n8n/nginx-entrypoint.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+# Process nginx template to substitute environment variables
 envsubst '$NGINX_ALLOWED_IP' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
 /usr/sbin/nginx -g "daemon off;"
