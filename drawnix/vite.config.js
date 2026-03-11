821b82656e75249f38b45bac70df53d4c18d873a
LeGitHubDeTai <65470169+LeGitHubDeTai@users.noreply.github.com>
2026-03-11 22:11:52 +0100

    ⬆️ Upgrade to 26.3.3

---
+++ drawnix/vite.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  server: {
+    host: '0.0.0.0',
+    port: 7200,
+    allowedHosts: [
+      'homeassistant.local',
+      'localhost',
+      '172.30.33.0',
+      '127.0.0.1'
+    ]
+  }
+};
