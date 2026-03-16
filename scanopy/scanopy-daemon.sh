1f43ba1e44c4f7c2237da3669992c8f8c58aae53
LeGitHubDeTai <65470169+LeGitHubDeTai@users.noreply.github.com>
2026-03-16 16:56:28 +0100

    26.3.4

--- scanopy/scanopy-daemon.sh
+++ scanopy/scanopy-daemon.sh
@@ -2,14 +2,12 @@
 set -e
 
 # Wait a bit for server to start
-sleep 5
+sleep 10
 
 echo "Starting Scanopy daemon..."
 
-# Start Scanopy daemon
+# Start Scanopy daemon using local binary
 exec /opt/scanopy/scanopy-daemon \
-    --log-level="${SCANOPY_LOG_LEVEL:-info}" \
     --server-url="${SCANOPY_SERVER_URL:-http://127.0.0.1:60072}" \
     --config-dir="/data/scanopy/daemon_config" \
-    --bind-address="0.0.0.0" \
-    --port=60073
+    --log-level="${SCANOPY_LOG_LEVEL:-info}"
